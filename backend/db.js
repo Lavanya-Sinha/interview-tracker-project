3003c4ff6517650ce4bf824c71f90837749bdde3
Lavanya-Sinha <lavanyasinha400@gmail>
2026-04-07 04:23:55 +0530

    fix : db variables

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql2")
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
+host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
