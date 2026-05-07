a9f1ae87b4af78c870cf2a292d2f6516dff3ba82
Lavanya-Sinha <lavanyasinha400@gmail>
2026-05-08 03:09:44 +0530

    fix : mysql pool connection

--- backend/db.js
+++ backend/db.js
@@ -1,17 +1,22 @@
-const mysql = require("mysql2")
-const db = mysql.createConnection({
-host: process.env.DB_HOST,
+const mysql = require("mysql2");
+
+const db = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
 });
-db.connect((err)=>{
-    if(err){
-        console.log("db connection failed",err)
-    }
-    else{
-        console.log("Connected to Mysql db")
-    }
-})
-module.exports = db
+
+// test connection
+db.getConnection((err, connection) => {
+  if (err) {
+    console.log("db connection failed", err);
+  } else {
+    console.log("Connected to Mysql db ✅");
+    connection.release();
+  }
+});
+
+module.exports = db;
