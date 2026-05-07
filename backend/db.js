6f803ff5a97dc9498463f9450079bb872c5c2f58
Lavanya-Sinha <lavanyasinha400@gmail>
2026-05-08 03:20:23 +0530

    fix : stablize mysql pool

--- backend/db.js
+++ backend/db.js
@@ -1,20 +1,21 @@
 const mysql = require("mysql2");
 
 const db = mysql.createPool({
-  connectionLimit: 10,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-// test connection
 db.getConnection((err, connection) => {
   if (err) {
     console.log("db connection failed", err);
   } else {
-    console.log("Connected to Mysql db ✅");
+    console.log("Connected to Mysql db ");
     connection.release();
   }
 });
