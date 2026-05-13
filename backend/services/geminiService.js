9c6c72361b848e9ace967df34443f68dc1b3db14
Lavanya-Sinha <lavanyasinha400@gmail>
2026-05-13 20:14:44 +0530

    fix : syntax

--- backend/services/geminiService.js
+++ backend/services/geminiService.js
@@ -1,5 +1,5 @@
-const { GoogleGenerativeAi } = require("@google/generative-ai")
-const genAi = GoogleGenerativeAi(process.env.GEMINI_API_KEY)
+const { GoogleGenerativeAI } = require("@google/generative-ai")
+const genAi = new GoogleGenerativeAI(process.env.GEMINI_API_KEY)
 
 const model = genAi.getGenerativeModel({
     model: "gemini-1.5-flash"
