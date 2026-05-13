c9b112b55fe0178a8a5d2fc51443f7726f092467
Lavanya-Sinha <lavanyasinha400@gmail>
2026-05-13 21:23:37 +0530

    model fix

--- backend/services/geminiService.js
+++ backend/services/geminiService.js
@@ -2,7 +2,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai")
 const genAi = new GoogleGenerativeAI(process.env.GEMINI_API_KEY)
 
 const model = genAi.getGenerativeModel({
-    model: "gemini-1.5-flash-latest"
+    model: "gemini-2.0-flash"
 });
 
 const generateInterviewQuestions = (topic,difficulty)=>{
