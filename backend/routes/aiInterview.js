790199cc9cac092906294dd6c5a4a530d8ae5e11
Lavanya-Sinha <lavanyasinha400@gmail>
2026-05-13 20:32:32 +0530

    bug fix

--- backend/routes/aiInterview.js
+++ backend/routes/aiInterview.js
@@ -3,23 +3,26 @@ const { generateInterviewQuestions } = require("../services/geminiService")
 const router = express.Router()
 
 router.post("/start",async(req,res)=>{
-    try {
         const{topic,difficulty} = req.body
-        return res.status(200).json({
-            success : true,
-            message : "Ai route is working",
-            topic,
-            difficulty,
-            question
-        })
-        
-    } catch (error) {
-        console.log("AI START ERROR : ",error);
-        return res.status(500).json({
-            success : false,
-            message : "Server error"
-        })
-        
-    }
+        generateInterviewQuestion(topic, difficulty)
+
+        .then((question) => {
+
+            return res.status(200).json({
+                success: true,
+                question
+            });
+
+        }) 
+         .catch((error) => {
+
+            console.log("AI START ERROR :", error);
+
+            return res.status(500).json({
+                success: false,
+                message: "Server error"
+            });
+
+        });
 })
 module.exports = router;
