ee80586129fd151331e0471e3668a3a0aab7e016
Lavanya-Sinha <lavanyasinha400@gmail>
2026-05-17 01:06:00 +0530

    added ai-history page

--- interview-tracker/src/pages/AIHistory.jsx
+++ interview-tracker/src/pages/AIHistory.jsx
@@ -25,9 +25,38 @@ const AIHistory = ()=>{
        }), []
     )
     return(
-      <div>
-          <h1> AI history Page</h1>
-      </div>    
+     <div className="min-h-screen bg-slate-900 text-white p-8">
+        <h1 className="text-4xl font-bold mb-8">
+           AI Interview History
+        </h1>
+
+    <div className="space-y-6">
+        {
+            sessions.map((session) => (
+                <div key={session.id} className="bg-slate-800 border border-slate-700 rounded-2xl p-6 space-y-3" >
+
+                    <h2 className="text-2xl font-semibold">
+                        {session.role}
+                    </h2>
+
+                    <p className="text-zinc-300">
+                        Difficulty: {session.difficulty}
+                    </p>
+
+                    <p className="text-zinc-400 text-sm">
+                        {
+                            new Date(
+                                session.created_at
+                            ).toLocaleString()
+                        }
+                    </p>
+                </div>
+            ))
+        }
+
+    </div>
+
+</div>
     )
 }
 export default AIHistory 
