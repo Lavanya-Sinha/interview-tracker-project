25db8677bf10149c260376d08be237ee721dcf02
Lavanya-Sinha <lavanyasinha400@gmail>
2026-05-17 22:40:09 +0530

    delete session

--- interview-tracker/src/pages/AIHistory.jsx
+++ interview-tracker/src/pages/AIHistory.jsx
@@ -1,12 +1,12 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import {useAuth} from '../hooks/useAuth'
-import {useToast} from '../hooks/useToast'
+import useToast from '../hooks/useToast'
 
 const AIHistory = ()=>{
     const {token} = useAuth()
     const navigate = useNavigate()
-    const {toast} = useToast()
+    const toast = useToast()
     const[sessions, setSessions] = useState([])
     useEffect(()=>{
         fetch( "https://interview-tracker-project.onrender.com/api/ai/sessions",
